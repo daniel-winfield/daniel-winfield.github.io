faa41dbf309e3e46cdd93a757e9264561a497505
Dan <daniel.winfield@outlook.com>
2020-01-23 23:17:13 +0000

    Added fontawesome. Added change to navigation bar on scroll. Formatted work/qualifications differently.

--- code.js
+++ code.js
@@ -5,9 +5,9 @@ var app = new Vue({
         jobTitle: 'Software Engineer',
         headerImageUrl: 'images/code-coding-computer-data-574070.jpg',
         socialLinks: [
-            { name: 'GitHub', url: 'https://github.com/daniel-winfield' },
-            { name: 'Twitter', url: 'https://twitter.com/danielwinfield_' },
-            { name: 'LinkedIn', url: 'https://www.linkedin.com/in/danielwinfield1/' }
+            { name: 'GitHub', url: 'https://github.com/daniel-winfield', iconClass: 'fa-github' },
+            { name: 'Twitter', url: 'https://twitter.com/danielwinfield_', iconClass: 'fa-twitter' },
+            { name: 'LinkedIn', url: 'https://www.linkedin.com/in/danielwinfield1/', iconClass: 'fa-linkedin-in' }
         ],
         about: {
             title: 'About me',
@@ -20,26 +20,42 @@ var app = new Vue({
                 { 
                     company: 'Imosphere Ltd',
                     jobTitle: 'Gradate Software Engineer',
-                    dates: 'July 2019 - Present',
-                    description: "Responsible for designing and implementing features and bug fixes on the Atmolytics healthcare software platform (C#, ASP.NET, TypeScript, SQL), and for developing for the Resource Allocation System (SQL) in use by councils and CCG's across the United Kingdom."
+                    dates: 'July 2019 - Present',                    
+                    startDate: new Date(2019, 6),
+                    endDate: null,
+                    description: "Responsible for designing and implementing features and bug fixes on the Atmolytics healthcare software platform (C#, ASP.NET, TypeScript, SQL), and for developing for the Resource Allocation System (SQL) in use by councils and CCG's across the United Kingdom.",
+                    companyImageUrl: '',
+                    location: 'Nottingham, UK'
                 },                
                 { 
                     company: 'Imosphere Ltd',
                     jobTitle: 'Placement Software Engineer',
                     dates: 'July 2017 - July 2019',
-                    description: "Worked on multiple projects during my year-long placement. This was continued in the same role during my final year of university on a one-day per week basis."
+                    startDate: new Date(2017, 6),
+                    endDate: new Date(2019, 6),
+                    description: "Worked on multiple projects during my year-long placement. This was continued in the same role during my final year of university on a one-day per week basis.",
+                    companyImageUrl: '',
+                    location: 'Nottingham, UK'
                 },
                 { 
                     company: 'One Tower Ltd',
                     jobTitle: 'Web Developer',
                     dates: 'May 2016 - Present',
-                    description: "Responsible for designing, implementing, and maintaining the website through the WordPress platform."
+                    startDate: new Date(2016, 4),
+                    endDate: null,
+                    description: "Responsible for designing, implementing, and maintaining the website through the WordPress platform.",
+                    companyImageUrl: '',
+                    location: 'Nottingham, UK'
                 },
                 { 
                     company: 'Blinds Fit',
                     jobTitle: 'Web and Social Media Developer',
                     dates: 'March 2016 - Present',
-                    description: 'Responsible for improving and updating the website through the Joomla platform.'
+                    startDate: new Date(2016, 2),
+                    endDate: null,
+                    description: 'Responsible for improving and updating the website through the Joomla platform.',
+                    companyImageUrl: '',
+                    location: 'Nottingham, UK'
                 }
             ]
         },
@@ -48,18 +64,54 @@ var app = new Vue({
             items: [
                 {
                     institution: 'Nottingham Trent University',
-                    dates: '2015-2019',
-                    info: 'BSc (Hons) Computer Science SW (First class honours)'
+                    startDate: new Date(2015, 0),
+                    endDate: new Date(2019, 0),
+                    qualification: 'BSc (Hons) Computer Science SW',
+                    info: 'Achieved a First class degree with honours',
+                    location: 'Nottingham, UK'
                 },{
                     institution: 'Burton and South Derbyshire College',
-                    dates: '2013-2015',
-                    info: 'Edexcel Extended Diploma in IT (Distinction Star, Distinction, Distinction)'
+                    startDate: new Date(2013, 0),
+                    endDate: new Date(2015, 0),
+                    qualification: 'Edexcel Extended Diploma in IT',
+                    info: 'Achieved a Distinction Star, Distinction, Distinction',
+                    location: 'Burton-on-Trent, UK'
                 },{
                     institution: 'The de Ferrers Academy',
-                    dates: '2007-2013',
-                    info: 'Achieved 9 GCSEs at grades A-C including Maths, English and Science.'
+                    startDate: new Date(2007, 0),
+                    endDate: new Date(2013, 0),
+                    qualification: 'GCSE',
+                    info: 'Achieved 9 GCSEs at grades A-C including Maths, English and Science.',
+                    location: 'Burton-on-Trent, UK'
                 }
             ]
+        },
+        months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    },
+    methods: {
+        handleScroll (event) {
+            console.log(document.body.scrollTop + ' ' + document.documentElement.scrollTop)
+            let navElement = document.getElementById("navbar");
+
+            if (document.documentElement.scrollTop > 50) {
+                navElement.classList.remove("navbar-light");
+                navElement.classList.remove("navbar-transparent");
+
+                navElement.classList.add("navbar-dark");
+                navElement.classList.add("bg-dark");
+            } else {
+                navElement.classList.remove("navbar-dark");
+                navElement.classList.remove("bg-dark");
+
+                navElement.classList.add("navbar-light");
+                navElement.classList.add("navbar-transparent");
+            }
         }
+    },
+    created () {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    destroyed () {
+        window.addEventListener('scroll', this.handleScroll);
     }
 })
